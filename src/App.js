fca4811b89a6b05ea8ac0d0c018a7ca806f1c2cd
cogito1016 <cogito1016@naver.com>
2020-05-25 16:45:47 +0900

    React study - Prop and Prop-types using npm install

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 function Food({name}){
   return <h1>I like {name}</h1>
 }
 
-const foodILike = ["Kimchi","Ramyun","Samgiopsal","chukumi"];
+Food.PropTypes = {
+  name:PropTypes.string.isRequired,
+};
+
+const foodILike = [
+  {
+    "id":1,
+    "name":"Kimchi"},
+  {
+    "id":2,
+    "name":"Ramyun"},
+  {
+    "id":3,
+    "name":"Samgiopsal"},
+  {
+    "id":4,
+    "name":"chukumi"}];
+
+
+function renderFood(dish){
+  return <Food key={dish.id} name={dish.name} />
+}//renderFood() end
 
 function App() {
   return (
     <div className="App"> 
     <h1>Hello</h1>
-    {foodILike.map(dish => {
-      return <Food name = {dish}/>
-    })}
+    {foodILike.map(renderFood)}
     </div>
   );
 }
