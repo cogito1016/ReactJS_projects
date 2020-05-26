d582dcf271d8153627272bd39cb7b2bba27831c9
cogito1016 <cogito1016@naver.com>
2020-05-26 21:36:53 +0900

    React Study - React Life Cycle - cons & render & mount & unmount & update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import React from 'react';
 //React Class Component
 class App extends React.Component {
 
+  constructor(props){
+    super(props);
+    console.log("I'm born"); //앱이 생성되었을때
+  }//cons end
+
   state = {
     count : 0
   };
@@ -15,7 +20,20 @@ class App extends React.Component {
     this.setState( current => ({ count : current.count-1}) );
   };
 
+  componentDidMount(){
+    console.log("Component Render") //컴포넌트가 최초 렌더링될때
+  }//comp~didmnt end 
+
+  componentDidUpdate(){
+    console.log("I just updated") //컴포넌트가 업데이트된 후
+  }//compdidudt end
+
+  componentWillUnmount(){
+    console.log("GoodBye.."); //컴포넌트가 죽었을때
+  }//compwiumnt end
+
   render(){
+    console.log("I am rendering") //컴포넌트가 그려졌을때
     return (
     <div>
       <h1>The number is {this.state.count}</h1>
