4c9ebc6f69e2cf5cec202ede2344e2b995c48539
cogito1016 <cogito1016@naver.com>
2022-01-27 23:29:00 +0900

    refactor: 달러입력 Input

--- movieApp/movie-app/src/App.js
+++ movieApp/movie-app/src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [property, setProperty] = useState(0);
 
   useEffect(() => {
     //한번만 실행할 예정
@@ -13,20 +14,41 @@ function App() {
         setLoading(false);
       });
   }, []);
+
+  const onChangePropertyHandler = (e) => {
+    setProperty(e.target.value);
+  };
+
+  console.log(property);
+
   return (
     <div>
-      <h1>The Coins!</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => {
-          //coin객체에 id가있기에 그것을 사용하기로함
-          return (
-            <li key={coin.id}>
-              {coin.name} ({coin.symbol}) = ${coin.quotes.USD.price}
-            </li>
-          );
-        })}
-      </ul>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <label htmlFor="property">Property : </label>
+          <input
+            id="property"
+            type="number"
+            value={property}
+            placeholder="Write your property"
+            onChange={onChangePropertyHandler}
+          />
+          <br />
+          <select>
+            {coins.map((coin) => {
+              //coin객체에 id가있기에 그것을 사용하기로함
+              return (
+                <option key={coin.id}>
+                  {coin.name} ({coin.symbol})
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      )}
     </div>
   );
 }
