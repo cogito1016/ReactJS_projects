c7c70fb93f7880487f4f671835e16cfb8ec5e5dd
cogito1016 <cogito1016@naver.com>
2022-02-02 20:44:10 +0900

    refactor: Async & Await 패턴으로 리팩톨링

--- movieApp/movie-app/src/App.js
+++ movieApp/movie-app/src/App.js
@@ -2,65 +2,36 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [property, setProperty] = useState(0);
-  const [selectedCoin, setSelectedCoin] = useState({
-    quotes: { USD: { price: 1 } },
-    symbol: "$",
-  });
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    //한번만 실행할 예정
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
 
-  const onChangePropertyHandler = (e) => {
-    setProperty(e.target.value);
-  };
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+    );
 
-  const onClickCoinsOptionHandler = (e) => {
-    console.log("함수실행");
-    console.log(e);
-    const selectedIndex = e.target.selectedIndex;
-    console.log(`INDEX : ${selectedIndex}`);
-    console.log(JSON.parse(e.target.value));
-    // setSelectedCoin(e.target[selectedIndex]);
+    const json = await response.json();
+
+    setLoading(false);
+    setMovies(json.data.movies);
+    console.log("First Start");
   };
 
   return (
     <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? (
-        <strong>Loading...</strong>
+      {loading === true ? (
+        <h1>Loading...</h1>
       ) : (
         <div>
-          <label htmlFor="property">Property : </label>
-          <input
-            id="property"
-            type="number"
-            value={property}
-            placeholder="Write your property"
-            onChange={onChangePropertyHandler}
-          />
-          <br />
-          <select onChange={onClickCoinsOptionHandler} value={selectedCoin}>
-            {coins.map((coin) => {
-              return (
-                <option key={coin.id} value={JSON.stringify(coin)}>
-                  {coin.name} ({coin.symbol})
-                </option>
-              );
+          <h1>Movies ({movies.length})</h1>
+          <ul>
+            {movies.map((element) => {
+              return <li key={element.id}>{element.title}</li>;
             })}
-          </select>
-          <br />
-          <h2>
-            = {property / selectedCoin.quotes.USD.price} {selectedCoin.symbol}
-          </h2>
+          </ul>
         </div>
       )}
     </div>
