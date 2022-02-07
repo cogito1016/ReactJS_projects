05137f675a29ce26f4609580c334a3e78746e2ef
cogito1016 <cogito1016@naver.com>
2022-02-07 23:45:32 +0900

    feat: Detail Movie도 구현 -방식은 이전과 동일

--- movieApp/movie-app/src/routes/Detail.js
+++ movieApp/movie-app/src/routes/Detail.js
@@ -1,10 +1,24 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Detail() {
   const { id } = useParams();
-  console.log(id);
+  const [movie, setMovie] = useState();
 
-  return <h1>Detail</h1>;
+  const getDetailMovie = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+    );
+    const json = await response.json();
+    const result = json.data.movie;
+    setMovie(result);
+  };
+
+  useEffect(() => {
+    getDetailMovie();
+  }, []);
+
+  return <h1>{movie.title}</h1>;
 }
 
 export default Detail;
